f119f5a0e0f5acbc08f31643e78b4ce50cdb51d6
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-01-09 13:01:05 -0800

    Top-Down Memoization is much prettier. :+1:

--- longestCommonSubsequence.js
+++ longestCommonSubsequence.js
@@ -1,38 +1,40 @@
 function longestCommonSubsequence(strOne, strTwo) {
-  let memo = initMemo(strOne.length + 1, strTwo.length + 1);
-  fillMemo(strOne.length - 1, strTwo.length - 1);
-  return extractLCS(strOne.length, strTwo.length, "");
+  let memo = initMemo(strOne.length, strTwo.length);
+  fillMemo(0, 0);
+  console.log(memo);
+
+  return extractLCS(0, 0, strTwo.length, "");
 
   function fillMemo(i, j) {
     if (i === strOne.length || j === strTwo.length) return 0;
-    if (memo[i + 1][j + 1] !== -1) return memo[i + 1][j + 1];
+    if (memo[i][j] !== -1) return memo[i][j];
     if (strOne[i] === strTwo[j]) {
-      memo[i + 1][j + 1] = 1 + fillMemo(i - 1, j - 1);
-      return memo[i + 1][j + 1];
+      memo[i][j] = 1 + fillMemo(i + 1, j + 1);
+      return memo[i][j];
     } else {
-      memo[i + 1][j + 1] = Math.max(fillMemo(i - 1, j), fillMemo(i, j - 1));
-      return memo[i + 1][j + 1];
+      memo[i][j] = Math.max(fillMemo(i, j + 1), fillMemo(i + 1, j));
+      return memo[i][j];
     }
   }
 
-  function extractLCS(row, col, seq) {
-    while (col > 0) {
-      while (memo[row][col - 1] === memo[row][col]) {
-        col--;
+  function extractLCS(row, col, colMax, seq) {
+    while (col < colMax) {
+      while (memo[row][col + 1] === memo[row][col]) {
+        col++;
       }
-      seq = strTwo[col - 1] + seq;
-      return extractLCS(row - 1, col - 1, seq);
+      seq += strTwo[col];
+      return extractLCS(row + 1, col + 1, colMax, seq);
     }
     return seq;
   }
 
   function initMemo(row, col) {
     let memo = new Array(row);
-    memo[0] = new Array(col).fill(0);
+    // memo[0] = new Array(col).fill(0);
 
-    for (let i = 1; i < row; i++) {
+    for (let i = 0; i < row; i++) {
       memo[i] = new Array(col).fill(-1);
-      memo[i][0] = 0;
+      // memo[i][0] = 0;
     }
 
     return memo;
